acd021dfe1cd3641e8698521b800c3f729d8f733
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-17 06:27:33 +0200

    feat: restructure Etap2Page component layout for improved readability and organization

--- app/etap2/page.js
+++ app/etap2/page.js
@@ -6,57 +6,55 @@ import ApartmentListCompact from "../components/ApartmentListCompact";
 
 const Etap2Page = () => {
     return (
-        <div className="home wp-singular page-template elementor-default elementor-page">
-            <main id="main" className="site-main">
-                <div>
-                    <section
-                        style={{
-                            padding: "40px 0",
-                        }}
-                    >
-                        <div className="elementor-container">
-                            <div className="elementor-column elementor-col-100">
-                                <div className="elementor-widget-wrap elementor-element-populated">
-                                    <div
-                                        style={{
-                                            textAlign: "center",
-                                            marginBottom: "20px",
-                                        }}
-                                    >
-                                        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-                                            Mieszkania Etap 2
-                                        </h1>
-                                        <p className="text-xl text-gray-600">
-                                            Przeglądaj dostępne mieszkania w
-                                            drugim etapie inwestycji Osiedle
-                                            Olimpijczyków.
-                                        </p>
-                                    </div>
-
-                                    <div className="mt-12 py-4">
-                                        <Suspense
-                                            fallback={<div>Loading...</div>}
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="home wp-singular page-template elementor-default elementor-page">
+                <main id="main" className="site-main">
+                    <div>
+                        <section
+                            style={{
+                                padding: "40px 0",
+                            }}
+                        >
+                            <div className="elementor-container">
+                                <div className="elementor-column elementor-col-100">
+                                    <div className="elementor-widget-wrap elementor-element-populated">
+                                        <div
+                                            style={{
+                                                textAlign: "center",
+                                                marginBottom: "20px",
+                                            }}
                                         >
+                                            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+                                                Mieszkania Etap 2
+                                            </h1>
+                                            <p className="text-xl text-gray-600">
+                                                Przeglądaj dostępne mieszkania w
+                                                drugim etapie inwestycji Osiedle
+                                                Olimpijczyków.
+                                            </p>
+                                        </div>
+
+                                        <div className="mt-12 py-4">
                                             <PropertySearch
                                                 stayOnPage={true}
                                                 showStage={false}
                                             ></PropertySearch>
-                                        </Suspense>
-                                    </div>
-                                    <div className="mt-6 bg-white mb-24">
-                                        <ApartmentListCompact
-                                            etap="2"
-                                            showSearch={false}
-                                            limit={50}
-                                        />
+                                        </div>
+                                        <div className="mt-6 bg-white mb-24">
+                                            <ApartmentListCompact
+                                                etap="2"
+                                                showSearch={false}
+                                                limit={50}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </section>
-                </div>
-            </main>
-        </div>
+                        </section>
+                    </div>
+                </main>
+            </div>
+        </Suspense>
     );
 };
 
