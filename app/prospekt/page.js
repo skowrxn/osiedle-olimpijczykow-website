9d2e7409cc961b67361cc71bc22e7a62d8c6547e
Bartłomiej <bartomiejskowron@icloud.com>
2025-10-01 11:07:35 +0200

    feat: `prospekt` page

---
+++ app/prospekt/page.js
@@ -0,0 +1,9 @@
+export default function ProspektPage() {
+    return (
+        <iframe
+            src="/NOWY-PROSPEKT-OLIMPIJCZYKOW-etap-II.pdf"
+            className="w-full min-h-screen border-0"
+            title="Prospekt"
+        />
+    );
+}
