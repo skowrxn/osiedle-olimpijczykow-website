4bdef46fe638da29f20a3eded11af43f83cf3e86
Bartłomiej <bartomiejskowron@icloud.com>
2025-10-08 17:53:34 +0200

    fix header

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -11,7 +11,7 @@ export default function Navigation() {
 
     return (
         <nav className="bg-white shadow-md px-4 py-6 border-b-1 border-neutral-200">
-            <div className="w-[1200px] container mx-auto flex justify-between items-center">
+            <div className="max-w-6xl container mx-auto flex justify-between items-center">
                 {/* Logo */}
                 <Link href="/" className="cursor-pointer">
                     <Image
